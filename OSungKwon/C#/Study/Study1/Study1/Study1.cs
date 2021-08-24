2f34fd32a05dfa0dd8686b89784fee9fea7f9639
KwonOsung <dyos1995@daum.net>
2021-08-24 18:46:07 +0900

    21. 08. 24 18:45 KwonOsung C# JAVA Study Code

---
+++ OSungKwon/C#/Study/Study1/Study1/Study1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Study1
+{
+    class Study1
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine(" 사용법 : HelloWorld.exe <Osung>");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", args[0]);
+        }
+
+    }
+}
