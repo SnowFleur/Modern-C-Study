f322aa0358654abc68ab74aa984eae7d8a72916c
SnowFleur <SnowFleur0128@gmail.com>
2021-09-22 00:07:46 +0900

    [SnowFleur] C#클라이언트 다시 추가

---
+++ DongHyukPark/Protocol Buffer(With Google)/Client(Csharp)/SnowNetSession.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace CSnowNetwork
+{
+    class CSnowNetSession
+    {
+        private Socket socket_;
+        public CSnowNetSession() { }
+
+        ~CSnowNetSession()
+        {
+            socket_.Close();
+        }
+
+        public bool ConnectToServer(string addr, int port)
+        {
+
+            socket_ = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Connect 
+            try
+            {
+                socket_.Connect(addr, port);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (socket_.Connected == true)
+            {
+                Console.WriteLine("The server connection was successful");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Can't Connection Server: Error:" + Marshal.GetLastWin32Error());
+                return false;
+            }
+        }
+
+        public bool SendMessage() { return true; }
+
+        public bool RecvMesage() { return true; }
+
+    }
+}
---
+++ DongHyukPark/Protocol Buffer(With Google)/Client(Csharp)/main.cs
@@ -0,0 +1,41 @@
+﻿/*
+-
+-   Proto Buffer 테스트용 C#(클라이언트) BasicSnowNetCore는 사용하지 않고 
+-   C#에서 제공하는 Net Socket 사용
+-
+*/
+
+using System;
+using System.Net.Sockets;
+using System.Net;
+using CSnowNetwork;
+
+namespace Client_Csharp_
+{
+
+    class Program
+    {
+        const string SERVER_ADDR    = "127.0.0.1";
+        const int PORT              = 9000;
+
+        CSnowNetSession session_;
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            pg.Start();
+        }
+
+        void Start() 
+        {
+            Console.WriteLine("Start!!");
+
+            session_ = new CSnowNetSession();
+            session_.ConnectToServer(SERVER_ADDR, PORT);
+
+            while (true) { };
+        
+        }
+
+    }
+}
